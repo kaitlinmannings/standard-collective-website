809eec878efc141ac3fd81c892a45b41170491a0
Kaitlin Mannings <dmannings@udallas.edu>
2024-05-30 15:03:30 -0500

    format updates

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
-document.querySelectorAll('.icon').forEach(icon => {
-    icon.addEventListener('click', (e) => {
-        const target = e.currentTarget.querySelector('p').textContent.toLowerCase() + '-window';
-        document.getElementById(target).style.display = 'block';
-    });
-});
+function openWindow(windowId) {
+    document.getElementById(windowId).style.display = 'block';
+}
 
 function closeWindow(windowId) {
     document.getElementById(windowId).style.display = 'none';
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const icons = document.querySelectorAll('.icon');
+    icons.forEach(icon => {
+        icon.addEventListener('click', () => {
+            const windowId = `${icon.textContent.trim().toLowerCase()}-window`;
+            openWindow(windowId);
+        });
+    });
+});
